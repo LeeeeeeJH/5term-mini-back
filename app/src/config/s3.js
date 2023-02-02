293d37b8b3e6c7c3cfb04d576fe16caf540feeca
jzzyy <passby158@gmail.com>
2023-02-02 17:52:02 +0900

    push 확인

--- app/src/config/s3.js
+++ app/src/config/s3.js
@@ -9,7 +9,7 @@ const s3 = new aws.S3({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
 });
-
+//s
 const upload = multer({
   storage: multerS3({
     s3: s3,
